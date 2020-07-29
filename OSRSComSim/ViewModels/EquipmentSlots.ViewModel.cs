8192d7e857a6e8b7f6d6aea9418789994e569eea
Dziugas Garla <dziugas.g99@gmail.com>
2020-07-29 17:01:25 +0300

    *Equipment improvement.

--- OSRSComSim/ViewModels/EquipmentSlots.ViewModel.cs
+++ OSRSComSim/ViewModels/EquipmentSlots.ViewModel.cs
@@ -9,17 +9,18 @@ using System.Windows.Navigation;
 
 namespace OSRSComSim.ViewModels
 {
-    public class EquipmentSlotsViewModel:ObservableObject
+    public class EquipmentSlotsViewModel : ObservableObject
     {
         private bool _show_select;
         private Equiped _player_equiped;
         private SelectEquipmentView _select_equipment;
         private string _equipment_info = "";
 
-        public SelectEquipmentView selectEquipment 
+        public SelectEquipmentView selectEquipment
         {
             get { return _select_equipment; }
-            set{
+            set
+            {
                 _select_equipment = value;
                 setEquipmentInfo();
                 OnPropertyChanged("selectEquipment");
@@ -27,7 +28,7 @@ namespace OSRSComSim.ViewModels
         }
         public Equiped PlayerEquiped
         {
-            get{ return _player_equiped; }
+            get { return _player_equiped; }
             set
             {
                 _player_equiped = value;
@@ -37,13 +38,12 @@ namespace OSRSComSim.ViewModels
         public string EquipmentInfo
         {
             get { return _equipment_info; }
-            set 
+            set
             {
                 _equipment_info = value;
                 OnPropertyChanged("EquipmentInfo");
             }
         }
-        public Position PositionEquipmentInfo { get; set; }
 
         public EquipmentSlotsViewModel(Equiped player_equiped = null, bool show_select = false)
         {
@@ -55,14 +55,13 @@ namespace OSRSComSim.ViewModels
             else PlayerEquiped = new Equiped();
             selectEquipment = null;
             _show_select = show_select;
-            setEqpInfoPos();
             setEquipmentInfo();
         }
 
         public void viewSelectEquipment(string selected_slot_table)
         {
-            if(_show_select)
-                selectEquipment = new SelectEquipmentView(PlayerEquiped,selected_slot_table);
+            if (_show_select)
+                selectEquipment = new SelectEquipmentView(PlayerEquiped, selected_slot_table);
         }
         public void stopSelectEquipment()
         {
@@ -73,72 +72,25 @@ namespace OSRSComSim.ViewModels
         public void setEquipmentInfo()
         {
             string Info = "";
-            Info = "SlashAtk "  + PlayerEquiped.getTotalSlashAtk() + "\n";
-            Info += "StabAtk "  + PlayerEquiped.getTotalStabAtk() + "\n";
+            Info = "SlashAtk " + PlayerEquiped.getTotalSlashAtk() + "\n";
+            Info += "StabAtk " + PlayerEquiped.getTotalStabAtk() + "\n";
             Info += "CrushAtk " + PlayerEquiped.getTotalCrushAtk() + "\n";
             Info += "MagicAtk " + PlayerEquiped.getTotalMagicAtk() + "\n";
-            Info += "RangedAtk "+ PlayerEquiped.getTotalRangedAtk() + "\n";
+            Info += "RangedAtk " + PlayerEquiped.getTotalRangedAtk() + "\n";
             Info += "SlashDef " + PlayerEquiped.getTotalSlashDef() + "\n";
-            Info += "StabDef "  + PlayerEquiped.getTotalStabDef() + "\n";
+            Info += "StabDef " + PlayerEquiped.getTotalStabDef() + "\n";
             Info += "CrushDef " + PlayerEquiped.getTotalCrushDef() + "\n";
             Info += "MagicDef " + PlayerEquiped.getTotalMagicDef() + "\n";
-            Info += "RangedDef "+ PlayerEquiped.getTotalRangedDef() + "\n";
-            Info += "MeleStr "  + PlayerEquiped.getTotalMeleStr() + "\n";
+            Info += "RangedDef " + PlayerEquiped.getTotalRangedDef() + "\n";
+            Info += "MeleStr " + PlayerEquiped.getTotalMeleStr() + "\n";
             Info += "MagicStr " + PlayerEquiped.getTotalMagicStr() + "\n";
-            Info += "RangedStr "+ PlayerEquiped.getTotalRangedStr() + "\n";
-            Info += "Prayer "   + PlayerEquiped.getTotalPrayer() + "\n";
-            Info += "Weigth "   + PlayerEquiped.getTotalWeigth() + "\n";
+            Info += "RangedStr " + PlayerEquiped.getTotalRangedStr() + "\n";
+            Info += "Prayer " + PlayerEquiped.getTotalPrayer() + "\n";
+            Info += "Weigth " + PlayerEquiped.getTotalWeigth() + "\n";
             EquipmentInfo = Info;
         }
 
-        private void setEqpInfoPos()
-        {
-            if (_show_select)
-            {
-                PositionEquipmentInfo = new Position
-                    (
-                        gridcol: 8,
-                        gridro: 1,
-                        colspan: 1,
-                        rospan: 7,
-                        W: 300,
-                        H: 400
-                    );
-            }
-            else
-            {
-                PositionEquipmentInfo = new Position
-                (
-                    gridcol: 1,
-                    gridro: 11,
-                    colspan: 7,
-                    rospan: 1,
-                    W: 500,
-                    H: 100
-                );
-            }
-        }
-    }
-
-
-    public struct Position
-    {
-        public int gridColumn { get; set; }
-        public int gridRow { get; set; }
-        public int columnSpan { get; set; }
-        public int rowSpan { get; set; }
-        public int Heigth { get; set; }
-        public int Width { get; set; }
-
-        public Position(int gridcol, int gridro, int colspan, int rospan, int H, int W)
-        {
-            gridColumn = gridcol;
-            gridRow = gridro;
-            columnSpan = colspan;
-            rowSpan = rospan;
-            Heigth = H;
-            Width = W;             
-        }
+        
     }
 
 }

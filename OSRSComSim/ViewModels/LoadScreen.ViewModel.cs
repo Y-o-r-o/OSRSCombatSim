3dc8f6c98c34d5e02be33ff7fc864191c8f21282
Dziugas Garla <dziugas.g99@gmail.com>
2020-08-16 20:15:56 +0300

    *Test

--- OSRSComSim/ViewModels/LoadScreen.ViewModel.cs
+++ OSRSComSim/ViewModels/LoadScreen.ViewModel.cs
@@ -17,7 +17,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+//asdfasfasfafas
 namespace OSRSComSim.ViewModels
 {
    public class LoadScreenViewModel: ObservableObject

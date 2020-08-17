dc50bdb0bd2a21f0e24d59ffc5fcfa1a27177eaf
Dziugas Garla <dziugas.g99@gmail.com>
2020-08-17 16:34:51 +0300

    *Removed unused libs.

--- OSRSComSim/Views/LoadScreen.View.xaml.cs
+++ OSRSComSim/Views/LoadScreen.View.xaml.cs
@@ -1,25 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using OSRSComSim.Models;
 using OSRSComSim.ViewModels;
 
 namespace OSRSComSim.Views
 {
-    /// <summary>
-    /// Interaction logic for LoadScreenView.xaml
-    /// </summary>
     public partial class LoadScreenView : UserControl
     {
         private LoadScreenViewModel view_model;
